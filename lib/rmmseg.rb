e6d1082527f8429b7eb9cbb072dde27f4473aad8
Howl王 <mimosa@shareday.com>
2013-02-15 20:03:35 +0800

    support 1.9 and jruby

---
+++ lib/rmmseg.rb
@@ -0,0 +1,13 @@
+# -*- encoding: utf-8 -*-
+require 'rmmseg/config'
+require 'rmmseg/simple_algorithm'
+require 'rmmseg/complex_algorithm'
+
+module RMMSeg
+  VERSION = '0.1.6'
+  
+  # Segment +text+ using the algorithm configured.
+  def segment(text)
+    Config.algorithm_instance(text).segment
+  end
+end
