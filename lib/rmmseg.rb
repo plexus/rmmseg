458a92378e6a53312da111169b63c7e3e6b0ab8e
Arne Brasseur <arne@arnebrasseur.net>
2013-06-18 11:50:40 +0200

    Split out RMMSeg::VERSION to its own file

--- lib/rmmseg.rb
+++ lib/rmmseg.rb
@@ -1,11 +1,11 @@
 # -*- encoding: utf-8 -*-
+
+require 'rmmseg/version'
 require 'rmmseg/config'
 require 'rmmseg/simple_algorithm'
 require 'rmmseg/complex_algorithm'
 
 module RMMSeg
-  VERSION = '0.1.6'
-  
   # Segment +text+ using the algorithm configured.
   def segment(text)
     Config.algorithm_instance(text).segment
---
+++ lib/rmmseg/version.rb
@@ -0,0 +1,3 @@
+module RMMSeg
+  VERSION = '0.1.6'
+end
